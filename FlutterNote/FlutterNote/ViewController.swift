36aa6483cb61d5c4a068bf53e466cf672e2cf486
WeirdPan <wooodypan@foxmail.com>
2019-12-26 20:52:18 +0800

    init Xcode proj

---
+++ FlutterNote/FlutterNote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlutterNote
+//
+//  Created by panwei on 2019/12/26.
+//  Copyright © 2019 WeirdPan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
